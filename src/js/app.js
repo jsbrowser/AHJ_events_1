5f393a98148c57ca7fd5d8e87bc74117f86d0fba
kate_gaw <ekaterinagaw@gmail.com>
2020-04-16 12:59:45 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -23,7 +23,7 @@ function getPoint() {
   const arr = [0, 1, 2, 3];
   const i = randomPointX(arr);
   const j = randomPointY(0, 3);
-  return (document.getElementById('table').childNodes[0].childNodes[i].cells[j]);
+  return (document.getElementById('table').rows[i].cells[j]);
 }
 
 function setPicture() {
